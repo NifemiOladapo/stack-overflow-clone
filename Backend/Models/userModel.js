e43db5fcecf1ddd918bda9b544a77648c15b4938
kollydap <dapkolly@gmail.com>
2022-12-02 21:57:39 +0100

    modified the configureDb and userModel file

--- Backend/Models/userModel.js
+++ Backend/Models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   picture: { type: String },
   createdOn: Date,
--- Backend/configureDb.js
+++ Backend/configureDb.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
-// mongodb+srv://nifnix:wkkcz0pgNo9i0fim@cluster0.aiour8y.mongodb.net/?retryWrites=true&w=majority
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost/stackoverflow", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://nifnix:wkkcz0pgNo9i0fim@cluster0.aiour8y.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log(`mongodb connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error.message);
